02f8c98d6437ec4fbd6f6370c601696ee20d3980
ajlopez <webmaster@ajlopez.com>
2018-10-22 19:08:47 -0300

    Add claim, WIP

--- contracts/ClaimHolder.sol
+++ contracts/ClaimHolder.sol
@@ -1,18 +1,46 @@
 pragma solidity ^0.4.24;
 
 contract ClaimHolder {
+    struct Claim {
+        uint256 topic;
+        uint256 scheme;
+        address issuer; // msg.sender
+        bytes signature; // this.address + claimType + data
+        bytes data;
+        string uri;
+    }
+    
+    mapping(bytes32 => Claim) claims;
+    
     function getClaim(bytes32 _claimId) 
         public 
         constant 
         returns(
-            uint256 claimType,
+            uint256 topic,
             uint256 scheme,
             address issuer,
             bytes signature,
             bytes data,
             string uri
         ) {
-        return (0, 0, address(0), new bytes(0), new bytes(0), "");
+        Claim storage claim = claims[_claimId];
+        
+        return (claim.topic, claim.scheme, claim.issuer, claim.signature, claim.data, claim.uri);
+    }
+    
+    function addClaim(
+        uint256 _topic, 
+        uint256 _scheme, 
+        address _issuer, 
+        bytes _signature, 
+        bytes _data, 
+        string _uri) returns (bytes32 claimRequestId) {
+        
+        bytes32 claimId = keccak256(abi.encodePacked(_issuer, _topic));
+        
+        claims[claimId] = Claim(_topic, _scheme, _issuer, _signature, _data, _uri);
+        
+        return claimId;
     }
 }
 
--- test/ClaimHolder_tests.js
+++ test/ClaimHolder_tests.js
@@ -6,7 +6,7 @@ contract('ClaimHolder', function (accounts) {
         this.claimHolder = await ClaimHolder.new();
     });
     
-    it('Unknown key has no purpose', async function() {
+    it('Get unknown claim', async function() {
         var result = await this.claimHolder.getClaim([]);
 
         assert.ok(Array.isArray(result));
@@ -14,10 +14,33 @@ contract('ClaimHolder', function (accounts) {
         assert.equal(result[0], 0);
         assert.equal(result[1], 0);
         assert.equal(result[2], 0);
-        console.dir(result[3]);
         assert.equal(result[3], '0x');
         assert.equal(result[4], '0x');
         assert.equal(result[5], "");
+    });
+    
+    it('Add claim', async function() {
+        await this.claimHolder.addClaim(
+            1,
+            2,
+            accounts[0],
+            [ 0x01 ],
+            [ 0x02 ],
+            "url"
+        );
+        
+        const claimId = web3.sha3(accounts[0].toString() + '01');
+
+        var result = await this.claimHolder.getClaim(claimId);
+
+        assert.ok(Array.isArray(result));
+        assert.equal(result.length, 6);
+        assert.equal(result[0], 1);
+        assert.equal(result[1], 2);
+        assert.equal(result[2], accounts[0]);
+        assert.equal(result[3], 1);
+        assert.equal(result[4], 2);
+        assert.equal(result[5], "url");
     });
 });
 
